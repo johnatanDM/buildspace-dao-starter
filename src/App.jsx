15b98ccc44c708bfdcc682bfbcdb933679510833
Johnatan Chaves Ribeiro <johnatan.jcr@gmail.com>
2021-12-21 17:04:24 -0300

    conectando carteira metamask

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { userEffect, useMemo, useState } from "react";
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to Web3 CommunityDAO</h1>
+        <h2>Or just CommunityDAO for the intimate</h2>
+        <h3>Came make the Web3 with us!</h3>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
